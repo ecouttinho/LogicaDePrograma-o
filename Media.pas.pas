c0b4bfe7a625aabd20316165ccbd829374671481
ecouttinho <147252785+ecouttinho@users.noreply.github.com>
2025-02-03 21:25:35 -0300

    Add files via upload

---
+++ Media.pas.pas
@@ -0,0 +1,12 @@
+program ProjetoSoma;
+var
+   a,b,soma: integer;
+
+begin
+     write('Digite o primeiro numero: ');
+     read(a);
+     write('Digite o primeiro numero: ');
+     read(b);
+     soma:= a + b;
+     wrete('O resultado da soma e: ', soma);
+end.
